1d3d7278dca28bb79e6f7626695850814ba709bb
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-07-02 11:46:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Products from "./pages/Products";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
 
 export default function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Products />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
