414cfce1a044a07b7f17ca127ba9d4a9990e2050
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-07-02 12:07:01 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
   return (
     <div>
       <h1>Worldwise</h1>
 
-      <link to="/pricing">Pricing</link>
+      <Link to="/pricing">Pricing</Link>
     </div>
   );
 }
