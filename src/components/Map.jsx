c0492e255e2c5381e8df3543f1553da0b524b280
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-07-03 19:09:53 +0530

    Update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,19 +13,18 @@ import { map } from "leaflet";
 import { useCities } from "../contexts/CitiesContext";
 import useGeolocation from "../hooks/UseGeolocation";
 import Button from "./Button";
+import useUrlPosition from "../hooks/useUrlPosition";
 
 export default function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
--- src/hooks/useUrlPosition.js
+++ src/hooks/useUrlPosition.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 export default function useUrlPosition() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return [lat, lng];
 }
